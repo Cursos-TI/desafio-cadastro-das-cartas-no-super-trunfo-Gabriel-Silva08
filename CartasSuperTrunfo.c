bb95eb7a37f70881f0e2767ef826b2e16d05e072
Gabriel-Silva08 <gabrielalesfc@gmail.com>
2025-05-22 01:50:17 +0000

    quase acabando o código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+int Carta;
+char Estado;
+char Código[150];
+char Cidade [150];
+int População;
+float Área;
+float PIB;
+int Pontos; 
+
+printf("Digite o nome do Estado /n");
+scanf("%s", &Estado);
+
+printf("Digite o Código da carta /n");
+scanf("%s", &Código);
+
+printf("Digite a Cidade /n");
+scanf("%s", &Cidade);
+
+printf("Digite o tamanho da população /n");
+scanf("%d",  &População);
+
+printf("Digite o tamanho da Área em km² /n");
+scanf("%f", &Área);
+
+printf("Digite o PIB /n");
+scanf("%f", &PIB);
+
+printf("Digite a quantidade de pontos turísticos /n");
+scanf("%d", &Pontos);
+
+printf("Carta: n");
+printf("Estado: %s /n", Estado  );
+printf("Código da carta: %s /n", Código);
+printf("Cidade: %s /n", Cidade);
+printf("População: %d /n", População);
+printf("Área(em km²): %f /n", Área);
+printf("PIB: %f /n", PIB);
+printf("Pontos Turísticos: %d /n", Pontos);
+
+return 0;
 }
